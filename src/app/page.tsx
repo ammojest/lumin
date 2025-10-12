5b11cc957a26781bba0932639b69a4edba503139
ammojest <tom.rhys.james@googlemail.com>
2025-10-12 23:02:32 +0100

    simplify Home component by removing client-side loading logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,12 @@
 "use client";
 
 import { TaskList } from "./components/TaskList";
-import { useState, useEffect } from "react";
 
 export default function Home() {
-  const [isClient, setIsClient] = useState(false);
-
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
-
   return (
     <div className="font-sans p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col row-start-2 items-center sm:items-start">
-        {isClient ? (
-          <TaskList />
-        ) : (
-          <div className="flex justify-center items-center min-h-screen">
-            <p>Loading Task Management Dashboard...</p>
-          </div>
-        )}
+        <TaskList />
       </main>
     </div>
   );
