67a4d9116b8361376f971c1a76dd9bb4cb721634
ammojest <tom.rhys.james@googlemail.com>
2025-10-12 22:40:04 +0100

    fix indentation in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-import Image from "next/image";
 import { TaskList } from "./components/TaskList";
 
 export default function Home() {
   return (
     <div className="font-sans p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col row-start-2 items-center sm:items-start">
-          <TaskList />
+        <TaskList />
       </main>
     </div>
   );
